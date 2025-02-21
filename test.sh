50c339b7bdc118cb123d299c74a672c175a02c6a
Ashish Tiwari <ashishjaitiwari15112000@gmail.com>
2025-02-21 19:22:03 +0530

    test pass

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 
 #create a none networked docker container and create it's network namespace.
 # This will be handled by the container runtime which will create and provide the container network namespace.
-docker container run -d --name test --network none alpine tail -f /dev/null
+cid=$(docker container run -d --name test --network none alpine tail -f /dev/null)
 # Get the container's PID
 pid=$(docker inspect -f '{{.State.Pid}}' test)
 sudo mkdir -p /var/run/netns
@@ -10,11 +10,11 @@ sudo ln -sf /proc/"$pid"/ns/net /var/run/netns/test
 
 
 # Execute CNI plugin
-CNI_COMMAND=ADD CNI_CONTAINERID=test CNI_NETNS=/var/run/netns/test CNI_IFNAME=eth0 CNI_ARGS="" CNI_PATH=./bin ./bin/ecni < conf/ebpf-cni.conf
+CNI_COMMAND=ADD CNI_CONTAINERID=$cid CNI_NETNS=/var/run/netns/test CNI_IFNAME=eth0 CNI_ARGS="" CNI_PATH=./bin ./bin/ecni < conf/ebpf-cni.conf
 
 #make assertion
-if ! docker container exec -it test ip addr show | grep -q eth0; then
-		docker container exec -it test ip addr show
+if ! docker container exec -i test ip addr show | grep -q eth0; then
+		docker container exec -i test ip addr show
     echo "No eth interface found, exiting."
     exit 1
 fi
@@ -23,8 +23,8 @@ fi
 # Execute CNI plugin
 CNI_COMMAND=DEL CNI_CONTAINERID=test CNI_NETNS=/var/run/netns/test CNI_IFNAME=eth0 CNI_ARGS="" CNI_PATH=./bin ./bin/ecni < conf/ebpf-cni.conf
 
-if docker container exec -it test ip addr show | grep -q eth0; then
-		docker container exec -it test ip addr show
+if docker container exec -i test ip addr show | grep -q eth0; then
+		docker container exec -i test ip addr show
     echo "eth interface found, exiting."
     exit 1
 fi
