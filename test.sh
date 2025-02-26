d5cbe411c894866b6bd19f803110730f8edd0012
Ashish Tiwari <ashishjaitiwari15112000@gmail.com>
2025-02-26 21:22:32 +0530

    run to completion by default

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 #create a none networked docker container and create it's network namespace.
 # This will be handled by the container runtime which will create and provide the container network namespace.
 cid=$(docker container run -d --name test --network none alpine tail -f /dev/null)
@@ -9,7 +10,7 @@ pid=$(docker inspect -f '{{.State.Pid}}' test)
 sudo mkdir -p /var/run/netns
 sudo ln -sf /proc/"$pid"/ns/net /var/run/netns/test
 
-
+: "${ENV:=prod}"
 # Execute CNI plugin
 CNI_COMMAND=ADD CNI_CONTAINERID=$cid CNI_NETNS=/var/run/netns/test CNI_IFNAME=eth0 CNI_ARGS="" CNI_PATH=./bin ./bin/ecni < conf/ebpf-cni.conf
 
@@ -20,12 +21,15 @@ if ! docker container exec -i test ip addr show | grep -q eth0; then
     exit 1
 fi
 
-echo "Breakpoint reached. Press ^C to continue..."
-trap "break" SIGINT
-while true; do
-    sleep 1
-done
-trap - SIGINT  # reset the SIGINT trap
+if [[ $ENV == "dev" ]]; then
+  echo "Breakpoint reached. Press ^C to continue..."
+  trap "break" SIGINT
+  while true; do
+      sleep 1
+  done
+  trap - SIGINT  # reset the SIGINT trap
+fi
+
 
 
 # Execute CNI plugin
